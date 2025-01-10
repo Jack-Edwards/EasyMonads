cc3e47259a2ec1f6fa927de7c8b6db654d50ead4
Jack Edwards <jackedwards@protonmail.com>
2025-01-10 13:35:59 -0600

    Add range support for IEnumerable<Task<Either<TLeft, TResult>>>

--- EasyMonads.Test/EitherTests/QueryTests/IEnumerableSelectTests.cs
+++ EasyMonads.Test/EitherTests/QueryTests/IEnumerableSelectTests.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using NUnit.Framework;
 
 namespace EasyMonads.Test.EitherTests.QueryTests
@@ -40,6 +41,42 @@ namespace EasyMonads.Test.EitherTests.QueryTests
          Either<Unit, string> ConvertToString(int number)
          {
             return number.ToString();
+         }
+      }
+      
+      [Test]
+      public async Task Select_Works_For_Async_IEnumerable_Either()
+      {
+         Dictionary<int, string> resultDict = new Dictionary<int, string>
+         {
+            { 0, "0" },
+            { 1, "1" },
+            { 2, "2" }
+         };
+         
+         IEnumerable<Either<Unit, string>> results = await from number in GetRangeAsync()
+            from text in ConvertToStringAsync(number)
+            select text;
+
+         foreach (var result in results.Select((x, i) => new { Value = x, Index = i }))
+         {
+            result.Value.DoRight(x => Assert.AreEqual(resultDict[result.Index], x));
+            result.Value.DoLeftOrNeither(Assert.Fail);
+         }
+
+         return;
+         
+         IEnumerable<Task<Either<Unit, int>>> GetRangeAsync()
+         {
+            foreach (var entry in resultDict)
+            {
+               yield return Either<Unit, int>.FromRight(entry.Key).AsTask();
+            }
+         }
+         
+         Task<Either<Unit, string>> ConvertToStringAsync(int number)
+         {
+            return Either<Unit, string>.From(number.ToString()).AsTask();
          }
       }
    }
--- EasyMonads/Either/EitherEnumerableExtensions.cs
+++ EasyMonads/Either/EitherEnumerableExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace EasyMonads
 {
@@ -15,6 +16,24 @@ namespace EasyMonads
       {
          return enumerableEither.Select<Either<TLeft, TRight>, Either<TLeft, TResult>>(either => either.Bind<TResult>(right =>
             bind(right).Bind<TResult>(intermediate => project(right, intermediate))));
+      }
+
+      public static IEnumerable<Task<Either<TLeft, TResult>>> Select<TLeft, TRight, TResult>(this IEnumerable<Task<Either<TLeft, TRight>>> enumerableEither, Func<TRight, TResult> map)
+      {
+         return enumerableEither.Select(async eitherTask => await eitherTask.MatchAsync(
+            left: Either<TLeft, TResult>.FromLeft,
+            right: right => map(right),
+            neither: Either<TLeft, TResult>.Neither));
+      }
+      
+      public static IEnumerable<Task<Either<TLeft, TResult>>> SelectMany<TLeft, TRight, TIntermediate, TResult>(this IEnumerable<Task<Either<TLeft, TRight>>> enumerableEitherTasks, Func<TRight, Task<Either<TLeft, TIntermediate>>> bind, Func<TRight, TIntermediate, TResult> project)
+      {
+         return enumerableEitherTasks.Select(async eitherTask => await eitherTask.BindAsync(async right =>
+            await bind(right).BindAsync(delegate (TIntermediate intermediate)
+            {
+               Either<TLeft, TResult> projection = project(right, intermediate);
+               return projection.AsTask();
+            })));
       }
    }
 }
